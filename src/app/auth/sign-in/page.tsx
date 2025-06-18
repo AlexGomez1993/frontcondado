54b1696035b3c022f162365e9b22c58449a5e527
Jean Sango <jeanpsv000@gmail.com>
2025-06-18 16:20:34 -0500

    feat: :sparkles: agregamos icono de condado y eliminamos metada innecesaria

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -6,8 +6,6 @@ import { GuestGuard } from '@/components/auth/guest-guard';
 import { Layout } from '@/components/auth/layout';
 import { SignInForm } from '@/components/auth/sign-in-form';
 
-export const metadata = { title: `Sign in | Auth | ${config.site.name}` } satisfies Metadata;
-
 export default function Page(): React.JSX.Element {
   return (
     <Layout>
